64facccc6fec941a498ad32349a49f8fd982d288
Alberto Campuzano <al.campuzano@shopify.com>
2020-02-28 11:52:24 -0500

    testing

--- Make tab red/background.js
+++ Make tab red/background.js
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 	
+	// testing
+	
 // Called when the user clicks on the browser action.
 	chrome.browserAction.onClicked.addListener(function(tab) {
 	  // No tabs or host permissions needed!
